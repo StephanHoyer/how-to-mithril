3727d788753ad5bb720fd5bcec4e7690a3a29998
Stephan Hoyer <ste.hoyer@gmail.com>
2018-09-05 16:15:04 +0200

    Refactor

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const baseUrl = document.location.pathname
 let examples = []
 const KEY_CODE_ESC = 27
+let currentFilter = {}
 
-function getHrefByFilter(filter) {
+function getRouteByFilter(filter) {
   return (
     (filter.tags.length ? `/tags/${filter.tags.join(',')}` : '') +
     (filter.q ? `/q/${filter.q}` : '') +
@@ -10,23 +11,23 @@ function getHrefByFilter(filter) {
   )
 }
 
-function tagView(tag, filter) {
-  return m('li.tag', linkView(filter, tag))
+function tagView(tag, filterUpdates) {
+  return m('li.tag', linkView(filterUpdates, tag))
 }
 
-function linkView({ className, tags, q, version }, children) {
+function linkView(filterUpdates, children) {
   return m(
     'a',
     {
-      className: className || '',
-      href: getHrefByFilter({ tags, q, version }),
+      className: filterUpdates.className || '',
+      href: getRouteByFilter(Object.assign({}, currentFilter, filterUpdates)),
       oncreate: m.route.link,
     },
     children
   )
 }
 
-function currentFilterView(currentFilter) {
+function currentFilterView() {
   return [
     m('input.q[autofocus]', {
       oncreate: ({ dom }) => setTimeout(dom.focus),
@@ -34,12 +35,12 @@ function currentFilterView(currentFilter) {
       onkeydown: e => {
         if (e.keyCode === KEY_CODE_ESC) {
           currentFilter.q = ''
-          m.route.set(getHrefByFilter(currentFilter))
+          m.route.set(getRouteByFilter(currentFilter))
         }
       },
       oninput: e => {
         currentFilter.q = event.target.value
-        m.route.set(getHrefByFilter(currentFilter))
+        m.route.set(getRouteByFilter(currentFilter))
       },
     }),
     currentFilter.q &&
@@ -47,7 +48,6 @@ function currentFilterView(currentFilter) {
         {
           className: 'clear',
           q: '',
-          tags: currentFilter.tags,
         },
         '×'
       ),
@@ -56,27 +56,25 @@ function currentFilterView(currentFilter) {
       currentFilter.tags.map(tag =>
         tagView(tag, {
           tags: currentFilter.tags.filter(t => tag !== t),
-          q: currentFilter.q,
         })
       )
     ),
     currentFilter.version &&
       linkView(
-        Object.assign({}, currentFilter, {
+        {
           className: 'version',
           version: '',
-        }),
+        },
         currentFilter.version
       ),
   ]
 }
 
-function exampleTagsView(example, currentFilter) {
+function exampleTagsView(example) {
   return m(
     'ul.tags',
     example.tags.map(tag =>
       tagView(tag, {
-        q: currentFilter.q,
         tags: currentFilter.tags.includes(tag)
           ? currentFilter.tags
           : currentFilter.tags.concat(tag),
@@ -85,33 +83,46 @@ function exampleTagsView(example, currentFilter) {
   )
 }
 
-function versionView(example, currentFilter) {
+function versionView(example) {
   return linkView(
-    Object.assign({}, currentFilter, {
+    {
       className: 'version',
       version: example.mithrilVersion,
-    }),
+    },
     example.mithrilVersion
   )
 }
 
-function matchesFilter(example, filter) {
+function versionView(example) {
+  return linkView(
+    {
+      className: 'version',
+      version: example.mithrilVersion,
+    },
+    example.mithrilVersion
+  )
+}
+
+function matchesFilter(example) {
   if (
-    filter.tags.length &&
-    filter.tags.some(tag => !example.tags.includes(tag))
+    currentFilter.tags.length &&
+    currentFilter.tags.some(tag => !example.tags.includes(tag))
   ) {
     return false
   }
   if (
-    filter.q &&
+    currentFilter.q &&
     example.tags
       .concat([example.name, example.description])
       .join('')
-      .indexOf(filter.q) < 0
+      .indexOf(currentFilter.q) < 0
   ) {
     return false
   }
-  if (filter.version && example.mithrilVersion !== filter.version) {
+  if (
+    currentFilter.version &&
+    example.mithrilVersion !== currentFilter.version
+  ) {
     return false
   }
   return true
@@ -122,36 +133,34 @@ const app = {
     examples = await m.request(`${baseUrl}examples.json`)
   },
   view: ({ attrs }) => {
-    const currentFilter = {
+    currentFilter = {
       tags: attrs.tags ? attrs.tags.split(',') : [],
       q: attrs.q || '',
       version: attrs.version,
     }
     return [
-      currentFilterView(currentFilter),
+      currentFilterView(),
       m(
         'ul.examples',
-        examples
-          .filter(example => matchesFilter(example, currentFilter))
-          .map(example =>
-            m(
-              'li.example',
-              {
-                title: example.description,
-              },
-              [
-                m(
-                  'a.name',
-                  {
-                    href: example.link,
-                  },
-                  example.name
-                ),
-                exampleTagsView(example, currentFilter),
-                versionView(example, currentFilter),
-              ]
-            )
+        examples.filter(example => matchesFilter(example)).map(example =>
+          m(
+            'li.example',
+            {
+              title: example.description,
+            },
+            [
+              m(
+                'a.name',
+                {
+                  href: example.link,
+                },
+                example.name
+              ),
+              exampleTagsView(example),
+              versionView(example),
+            ]
           )
+        )
       ),
     ]
   },
